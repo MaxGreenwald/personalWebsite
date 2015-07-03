6c86bede5b85ed85594d70222277b27933e5f53f
MaxGreenwald <maxpg@princeton.edu>
2015-07-03 13:54:58 -0400

    personal site test2

--- flaskServer.py
+++ flaskServer.py
@@ -1,9 +1,14 @@
 from flask import Flask, send_from_directory, render_template
 app = Flask(__name__)
+import settings
 
 @app.route('/')
 def home():
     return send_from_directory('templates', 'home.html')
 
+@app.route('/blog')
+def home():
+    return send_from_directory('templates', 'blog.html')
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(settings.HOST, debug = settings.DEBUG)
